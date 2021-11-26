5b84249e7039853e4675aff617d1daff6a45b988
Jeanndhet <75793756+Jeanndhet@users.noreply.github.com>
2021-11-25 23:56:02 -0500

    Add files via upload

---
+++ about.php
@@ -0,0 +1,88 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Oswald:200,600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/styles_about.css">
+
+    <title>Renta de Peliculas</title>
+  </head>
+  <style>*{background-color: rgba(8,15,40,60); color: whitesmoke;}</style>
+  <body background="#050505">
+    <section>
+        <div class="row no-gutters">
+            <div class="col-lg-1">
+                <div class="d-flex h-100">
+                    <div class="align-self-center mx-auto">
+                        <ion-icon name="search-outline"></ion-icon>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-11">
+                <nav class="navbar navbar-expand-lg">
+                    <a class="navbar-brand text-dark" href="#"> <span class="font-weight-bold">Pelis Plus</span></a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                      <span class="navbar-toggler-icon"></span>
+                    </button>
+                  
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                      <ul class="navbar-nav ml-auto">
+                        <li class="nav-item active">
+                          <a class="nav-link text-dark lead" href="#"><p>Representantes</p></a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link text-dark lead" href="rentings.php"><p>Rentas</p></a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link text-dark lead" href="#"><p>Membresía</p></a>
+                        </li>  
+                      </ul>
+                    </div>
+                  </nav>
+            </div>
+        </div>
+
+        <div class="row no-gutters">
+            <div class="col-lg-1">
+                <div class="d-flex">
+                    <ul class="list-unstyled align-self-center mx-auto">
+                        <li class="sideways"><a href="#" class="text-dark"><span>Instagram</span></a></li>
+                        <li class="sideways"><a href="#" class="text-dark"><span>Facebook</span></a></li>
+                        <li class="sideways"><a href="#" class="text-dark"><span>Youtube</span></a></li>
+                        <li class="sideways"><a href="#" class="text-dark"><span>Dribble</span></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-4 d-flex">
+                <div class="container align-self-center px-5">
+                    <h1 class="text-uppercase font-weight-bold mb-4">Acerca de Pelis Plus</h1>
+                    <p class="lead-xl mb-4">Pelis Plus  es una página que fue creada para poder tener el beneficio de un cine pero en la comodidad de tu hogar,
+                        asi como tambien poder escoger por el género. Es un medio conocido en Perú
+                        por la calidad de imagen y la interfaz muy llamativa y sencilla. Así mismo en la pagina se puede crear un usuario, pagar una membresía y obtener beneficios.</p>
+                    <a href="home.php" class="btn btn-primary">Renta ahora!</a>    
+                </div>
+            </div>
+            <div class="col-lg-7">
+                <img src="img/bbpel.jpg" class="img-fluid">
+            </div>
+        </div>
+    </section>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
--- bd.php
+++ bd.php
@@ -1,3 +1,4 @@
 <?php
 $conexion = mysqli_connect("localhost", "root", "", "lab13");
 mysqli_set_charset($conexion, "utf8");
+
